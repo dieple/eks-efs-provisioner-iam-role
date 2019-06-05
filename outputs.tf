6ca325e710987608b2177e7b84ffc0d562974436
Diep Le <diep.le@river-island.com>
2019-06-05 14:37:39 +0100

    renamed output fields

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "role_name" {
-  value       = "${module.eks_efs_provisioner_iam_role.role_name}"
+output "name" {
+  value       = "${module.eks_efs_provisioner_iam_role.name}"
   description = "The name of the created role"
 }
 
-output "role_id" {
-  value       = "${module.eks_efs_provisioner_iam_role.role_id}"
+output "id" {
+  value       = "${module.eks_efs_provisioner_iam_role.id}"
   description = "The stable and unique string identifying the role"
 }
 
-output "role_arn" {
-  value       = "${module.eks_efs_provisioner_iam_role.role_arn}"
+output "arn" {
+  value       = "${module.eks_efs_provisioner_iam_role.arn}"
   description = "The Amazon Resource Name (ARN) specifying the role"
 }
 
-output "role_policy" {
-  value       = "${module.eks_efs_provisioner_iam_role.role_policy}"
+output "policy" {
+  value       = "${module.eks_efs_provisioner_iam_role.policy}"
   description = "The Amazon Resource Name (ARN) specifying the role"
 }
